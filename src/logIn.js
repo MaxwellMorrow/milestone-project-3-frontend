a2fe46832354543bd8142a8d496b3490426cc1cc
Maxwell Morrow <mmorrow@ad.boomtownroi.com>
2022-07-26 23:25:34 -0400

    added handlesubmit function for login

--- src/logIn.js
+++ src/logIn.js
@@ -1,6 +1,31 @@
 import './App.css';
 
 function logIn() {
+  
+
+  // submit function send token to backend if its in local storage
+  async function handleSubmit(e) {
+      const response = await fetch(`http://localhost:5000/authentication/`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(credentials)
+      })
+  
+      const data = await response.json()
+  
+      if (response.status === 200) {
+          setCurrentUser(data.user)
+          localStorage.setItem('token', data.token)
+          history.push(`/`)
+      } else {
+          setErrorMessage(data.message)
+      }}
+  
+
+
+
   return (
     <div class="entry">
         <label for="user">Username</label>
